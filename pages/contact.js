3d9cb368e0587bfbb9ada274c78e39d00c37fcaa
Molly Jensen <92386889+moy864@users.noreply.github.com>
2022-03-24 22:43:05 +0000

    create contact page

---
+++ pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+const ContactPage = () => {
+ const ContactForm = 
+ 
+ return (
+   <div>
+     <h1>Get in touch!</h1>
+     {ContactForm}
+   </div>
+ );
+};
+ 
+export default ContactPage;
