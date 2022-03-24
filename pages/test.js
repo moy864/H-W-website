43d4a44778a9e3c7176f3257225fdf8d822fecd4
Molly Jensen <92386889+moy864@users.noreply.github.com>
2022-03-24 22:58:04 +0000

    pages layout

---
+++ pages/test.js
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+function Test() {
+  return <div>About</div>
+}
+
+export default Test 
