267e26242db740ee648aa1eca01bc8374f5ca1a9
Molly Jensen <92386889+moy864@users.noreply.github.com>
2022-03-25 12:59:56 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <br>
+          Welcome to 
             <span className={"grad"}>
                          Thinking Forwards          
             </span>
