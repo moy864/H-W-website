87588ecbf753579beea639ffba452f8f9484c591
Molly Jensen <{mollyjensen05@gmail.com}>
2022-03-27 15:29:38 +0100

    why so many brackets

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -45,7 +45,7 @@ export const Navbar = () => {
         >
           <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
             <Link href='/'>
-              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white '>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
                 Home
               </a>
             </Link>
