0c9b09aeba79f1096c8e4a61a4c4bbba2aa2300f
Molly Jensen <{mollyjensen05@gmail.com}>
2022-03-27 11:20:32 +0100

    index.js update

--- pages/index.js
+++ pages/index.js
@@ -26,18 +26,7 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <p> All content
-                &copy; 2022<br />
-                Divyakshi Goyal and Molly Jensen</p>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+                &copy; 2022  Divyakshi Goyal and Molly Jensen</p>
       </footer>
     </div>
   )
