c07965630183a0f98de44797c6363a2241f923eb
Matthew Savin <matt.asavin@gmail.com>
2022-04-02 20:20:36 +0100

    refactor to 4 space indentation

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,4 @@
-import '../styles/main.css';
+import '../styles/globals.css';
 import Layout from '../components/layout';
 import React from "react";
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  content: [],
+  content: [
+    '/styles/globals.css'
+  ],
   presets: [],
   darkMode: 'media', // or 'class'
   theme: {
